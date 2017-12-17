9287518616febac98cfdfb2c9341e8579b8b4dfc
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-12-18 01:14:39 +0530

    Inception v3(CoreML model) imported

---
+++ JinYangCFOOD/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JinYangCFOOD
+//
+//  Created by   Rastogi on 18/12/17.
+//  Copyright © 2017 Vinayak Rastogi. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
+
