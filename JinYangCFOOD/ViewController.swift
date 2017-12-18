fb557c10b5a390c1d32e4f7c6c2ba70eade2e7d2
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-12-18 15:55:34 +0530

    added 2 additional delegate protocols and created IB action and outlet for camera and imageView

--- JinYangCFOOD/ViewController.swift
+++ JinYangCFOOD/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
 }
 
