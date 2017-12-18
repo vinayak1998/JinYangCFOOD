63c747186d0176bd15ded7060978d8fe41f26e14
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-12-18 19:53:37 +0530

    The method to process and get a classification out of the coreimage image added

--- JinYangCFOOD/ViewController.swift
+++ JinYangCFOOD/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
         
         //setting other properties
-        imagePicker.sourceType = .camera  //brings up a UIImagePicker that contains the camera module to allow the user to take an image using the front or rear camera
+        imagePicker.sourceType = .photoLibrary  //brings up a UIImagePicker that contains the camera module to allow the user to take an image using the front or rear camera
         imagePicker.allowsEditing = false   //boolean val. whether the user is allowed to edit a selected image or movie.
 
         
@@ -43,10 +43,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage //downcasting to image datatype
         {
             imageView.image = userPickedImage
+            
+            //now to convert ui image to ci(core image) image, allows us to use the vision framework on the image
+            //extra security feature to make the code safer
+
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            
+            detect(image: ciimage) // calling the most exquisite method in the code
         }
         
         //finally we're gonna dismiss that imagepicker and go back to our viewcontroller
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    //after conversion of ciimage to uiimage, we need a method to process the ciimage and get a classification out of it
+    func detect(image: CIImage){
+        
+        //using the inception v3 model now,
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model)  //a container for a coreML model used for vision requests
+            else{
+                fatalError("Loading CoreML Model Failed")
+        }
+        
+        //now we're going to access this model object that we created and create a viosion coreML request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            //code completion request that is when that request is completed, to process the results of that request
+            // create object results to do the above
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        // creating a handlrer that specifies the image to be calssified
+        let handler = VNImageRequestHandler(ciImage: image) //image is the parameter name of the detect func
+        
+        do{
+            try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
+    
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
